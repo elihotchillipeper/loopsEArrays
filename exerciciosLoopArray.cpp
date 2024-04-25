04f826ad96ff64cbdddd84aef6ab442b801dc9d7
elihotchillipeper <150726982+elihotchillipeper@users.noreply.github.com>
2024-04-25 22:50:49 +0000

    exercícios loop em array

---
+++ exerciciosLoopArray.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+int exe01(){
+
+    //Declare um array de inteiros com 5 elementos e preencha-o com valores de sua escolha. Em seguida, utilize um loop for para imprimir cada elemento do array no terminal.
+
+    int array[5] = {1, 2, 3, 4, 5};
+
+    for(int i = 0; i < 5; i++){
+        cout << array[i] <<std::endl;
+    }
+
+    return 0;
+}
+
+
+int exe02(){
+
+    //Declare um array de caracteres com 6 elementos e preencha-o com letras do alfabeto. Utilize um loop while para imprimir cada elemento do array no terminal.
+
+    char array[6] = {'e', 'l', 'i', 's', 'a', '!'};
+
+    for(int i = 0; i < 6; i++){
+        cout << array[i] <<std::endl;
+    }
+
+    return 0;
+}
+
+
+int exe03(){
+
+    //Declare um array de inteiros com 7 elementos e preencha-o com números de sua escolha. Em seguida, utilize um loop for para imprimir apenas os números pares do array.
+
+    int array[7] = {1, 2, 3, 4, 5, 6, 7};
+
+    for(int i = 1; i < 7; i++){
+        cout << array[i] <<std::endl;
+        i++;
+    }
+
+    return 0;
+}
+
+
+int exe04(){
+
+    //Declare um array de floats com 8 elementos e preencha-o com valores decimais. Utilize um loop while para imprimir apenas os valores maiores que 5 do array.
+
+    float array[8] = {1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5};
+
+    while
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+int main(){
+    //exe01();
+    //exe02();
+    //exe03();
+    exe04();
+
+
+
+
+}
